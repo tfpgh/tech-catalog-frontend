e50ba6a27007426f0d2a338f461a457684c8f361
Toby Penner <toby.penner@theloganschool.org>
2022-01-12 10:40:55 -0700

    Add google analytics

--- src/App.svelte
+++ src/App.svelte
@@ -12,6 +12,27 @@
 			})
 			.join(" ");
 	}
+	let theme = localStorage.getItem("theme");
+
+	// Assign theme if necessary
+	if (theme === null) {
+		theme = Math.floor(Math.random() * 3) + 1; // 1-3
+		localStorage.setItem("theme", theme);
+	}
+	if (theme != "1") {
+		const top = document.getElementById("top");
+
+		top.classList.add("theme-" + theme);
+		top.classList.remove("theme-1");
+	}
+
+	let measurement_id = "G-9RBCKH0VZC";
+
+	if (theme === "2") {
+		measurement_id = "G-M1C33DTF1P";
+	} else if (theme === "3") {
+		measurement_id = "G-EK34GRCELG";
+	}
 
 	let shown_category = "all";
 
@@ -37,22 +58,23 @@
 			"https://tech-catalog-backend.herokuapp.com/get_items"
 		);
 		items = await res.json();
-
-		// Assign theme if necessary
-		let theme = localStorage.getItem("theme");
-		if (theme === null) {
-			theme = Math.floor(Math.random() * 3) + 1; // 1-3
-			localStorage.setItem("theme", theme);
-		}
-		if (theme != "1") {
-			const top = document.getElementById("top");
-
-			top.classList.add("theme-" + theme);
-			top.classList.remove("theme-1");
-		}
 	});
 </script>
 
+<svelte:head>
+	<script
+		async
+		src="https://www.googletagmanager.com/gtag/js?id={measurement_id}"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag("js", new Date());
+
+		gtag("config", measurement_id);
+	</script>
+</svelte:head>
 <div id="top" class="defaults theme-1">
 	<div id="nav-bar">
 		{#each categories as category}
