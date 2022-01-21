7621c1ba84bc4e3fa7c5e5137a596889e75e82e1
Toby Penner <tobypenner01@gmail.com>
2022-01-20 21:59:13 -0700

    Correct image margin

--- src/Item.svelte
+++ src/Item.svelte
@@ -1,98 +1,100 @@
 <script>
-    import { onMount } from "svelte";
+  import { onMount } from "svelte";
 
-    export let name = "Default Name";
-    export let description = "Default Description";
-    export let quantity = "42";
-    export let rotation = 0;
-    export let checkoutable = true;
-    export let image_url = "https://via.placeholder.com/720";
+  export let name = "Default Name";
+  export let description = "Default Description";
+  export let quantity = "42";
+  export let rotation = 0;
+  export let checkoutable = true;
+  export let image_url = "https://via.placeholder.com/720";
 
-    let image;
+  let image;
+  let image_margin = 0;
 
-    function updateRotation() {
-        image.style.transform = "rotate(" + rotation * 90 + "deg)";
-        if (rotation % 2 === 0) {
-            image.style.margin = "0px 0px";
-        } else {
-            let offset = image.width - image.height;
-            image.style.margin = offset / 2 + "px 0px";
-        }
+  function updateRotation() {
+    image.style.transform = "rotate(" + rotation * 90 + "deg)";
+    if (rotation % 2 === 0) {
+      image_margin = 0;
+    } else {
+      image_margin = (image.width - image.height) / 2;
     }
+  }
 
-    onMount(() => {
-        updateRotation();
-    });
+  onMount(() => {
+    updateRotation();
+  });
 </script>
 
 <div>
-    <span>
-        {#if !checkoutable}
-            <h1 id="name" class="flex-basis-100 margin-tb">
-                {name}<span id="asterisk">*</span>
-            </h1>
-        {:else}
-            <h1 id="name" class="flex-basis-100 margin-tb">{name}</h1>
-        {/if}
-        <p id="description" class="flex-basis-100 margin-tb">{description}</p>
-        <p id="quantity" class="flex-basis-100 margin-tb">
-            Quantity: {quantity}
-        </p>
-    </span>
-    <img
-        src={image_url}
-        alt={description}
-        class="flex-basis-100 margin-tb"
-        bind:this={image}
-    />
+  <span>
+    {#if !checkoutable}
+      <h1 id="name" class="flex-basis-100 margin-tb">
+        {name}<span id="asterisk">*</span>
+      </h1>
+    {:else}
+      <h1 id="name" class="flex-basis-100 margin-tb">{name}</h1>
+    {/if}
+    <p id="description" class="flex-basis-100 margin-tb">{description}</p>
+    <p id="quantity" class="flex-basis-100 margin-tb">
+      Quantity: {quantity}
+    </p>
+  </span>
+  <img
+    src={image_url}
+    alt={description}
+    class="margin-tb"
+    width="364"
+    height="274"
+    style="margin: {image_margin}px 50%;"
+    bind:this={image}
+  />
 </div>
 
 <style>
-    div {
-        background-color: var(--secondary-background-color);
-        width: 380px;
-        text-align: center;
-        border-radius: var(--corner-radius);
-        border-style: solid;
-        border-color: var(--border-color);
-        border-width: 3px;
-        padding: 15px;
-        padding-bottom: 20px;
-        padding-top: 0;
-        margin: 15px;
-        flex-grow: 1;
-        display: flex;
-        flex-wrap: wrap;
-        align-items: center;
-        justify-content: center;
-    }
+  div {
+    background-color: var(--secondary-background-color);
+    width: 380px;
+    text-align: center;
+    border-radius: var(--corner-radius);
+    border-style: solid;
+    border-color: var(--border-color);
+    border-width: 3px;
+    padding: 15px;
+    padding-bottom: 20px;
+    padding-top: 0;
+    margin: 15px;
+    flex-grow: 1;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
 
-    .flex-basis-100 {
-        flex-basis: 100%;
-    }
+  .flex-basis-100 {
+    flex-basis: 100%;
+  }
 
-    .margin-tb {
-        margin-top: 0.4rem;
-        margin-bottom: 0.4rem;
-    }
+  .margin-tb {
+    margin-top: 0.4rem;
+    margin-bottom: 0.4rem;
+  }
 
-    h1 {
-        color: var(--main-text-color);
-        font-family: var(--main-font);
-    }
+  h1 {
+    color: var(--main-text-color);
+    font-family: var(--main-font);
+  }
 
-    p {
-        color: var(--secondary-text-color);
-        font-family: var(--secondary-font);
-    }
+  p {
+    color: var(--secondary-text-color);
+    font-family: var(--secondary-font);
+  }
 
-    img {
-        max-width: 95%;
-        border-radius: var(--corner-radius);
-    }
+  img {
+    border-radius: var(--corner-radius);
+  }
 
-    #asterisk {
-        color: #ff4136;
-        font-size: 2rem;
-    }
+  #asterisk {
+    color: #ff4136;
+    font-size: 2rem;
+  }
 </style>
